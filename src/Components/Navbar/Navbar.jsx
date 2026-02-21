485a11d41017a83c3dc0f232c0e559bf40e41827
MadeByKhush <khushsharmacollage@gmail.com>
2026-02-21 14:44:22 +0530

    logo and hero optimized

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../assets/logo-upscaled.png";
+import logo from "../../assets/logo-upscaled.webp";
 import AdmissionModal from "../AdmissionModal/AdmissionModal";
 
 const Navbar = () => {
